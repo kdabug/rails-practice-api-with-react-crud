20799d116f0c571812010952bce49c50a17b3be3
Mikayda Mills <mills.ma12@gmail.com>
2019-09-02 08:13:37 -0400

    edit student controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,20 @@
 class StudentsController < ApplicationController
+   
+    def index
+      @students = Student.all
+      render json: { message: "ok", students: @students }
+    end
+  
+    def show
+      begin
+        @student = Student.find(params[:id])
+        render json: { message: "ok", student: @student }
+      rescue ActiveRecord::RecordNotFound
+        render json: { message: 'no student matches that ID' }, status: 404
+      rescue StandardError => e
+        render json: { message: e.to_s }, status: 500
+      end
+    end
+  
+
 end
