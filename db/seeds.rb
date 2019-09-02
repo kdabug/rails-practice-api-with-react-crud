8ff8ceaa2750bf2dfe1fe62f521086b2a214a355
Mikayda Mills <mills.ma12@gmail.com>
2019-09-02 08:09:06 -0400

    add routes and controller for students

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@ Student.create!(name: 'Gerald Martin Johanssen', description: 'Athletic, loyal, 
 Student.create!(name: 'Helga G. Pataki', description: 'rough, cynical, favorite color is pink, closet poet, expert shrine-maker', photo: 'https://heyarnold.fandom.com/wiki/Helga_G._Pataki?file=Helga_Pataki.png')
 Student.create!(name: 'Harold Berman', description: 'tough-talker, bully, body-shamed, acts out, relatively harmless', photo: 'https://heyarnold.fandom.com/wiki/Harold_Berman?file=Upscale-230765195014212.png')
 Student.create!(name: 'Eugene Horowitz', description: 'accident prone, cheery, the class jinx, wears socks with sandals', photo: 'https://heyarnold.fandom.com/wiki/Eugene_Horowitz?file=EAFDB121-EC13-4F12-849E-37037A95E3D6.png')
-Student.create!(name: 'Phoebe Heyerdahl', description: 'smartest person in the room, shy, makes 'long sweater' an outfit', photo: 'https://heyarnold.fandom.com/wiki/Phoebe_Heyerdahl?file=Phoebe_(classic).png')
+Student.create!(name: 'Phoebe Heyerdahl', description: 'smartest person in the room, shy, makes "long sweater" an outfit', photo: 'https://heyarnold.fandom.com/wiki/Phoebe_Heyerdahl?file=Phoebe_(classic).png')
 
 puts "#{Student.count} students created!"
