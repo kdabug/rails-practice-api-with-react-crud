815d2338a7014b6472424d432d82551c21bab509
Mikayda Mills <mills.ma12@gmail.com>
2019-09-03 12:02:22 -0400

    install packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  state = {
+    students: [],
+    selectedStudent: null
+  };
+
+  componentDidMount() {
+    console.log("hi");
+    axios
+      .get("http://localhost:3000/students", {
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        }
+      })
+      .then(response => {
+        this.setState({
+          students: response.data.students
+        });
+      });
+  }
+
+  selectStudent = student => {
+    this.setState({
+      selectedStudent: student
+    });
+  };
+
+  students = () => {
+    return this.state.students.map(student => (
+      <div key={student.id}>
+        <Link
+          to={`/students/${student.id}`}
+          onClick={() => this.selectStudent(student)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {student.name}
+        </Link>
+        <hr />
+      </div>
+    ));
+  };
+
+  student = () => {
+    const student = this.state.selectedStudent;
+    return (
+      <div className="c-card">
+        <img alt={student.name} src={student.photo} width="20%" />
+        <h1>{student.name}</h1>
+        <p>{student.description}</p>
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Link exact="true" to="/">
+            Student App
+          </Link>
+          <Switch>
+            <Route exact path="/" render={this.students} />
+            <Route exact path="/students/:id" render={this.student} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
